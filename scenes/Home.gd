89a8956a898f8c993f87a8ec233c123a52fff430
Loweeest <128013080+Loweeest@users.noreply.github.com>
2025-02-17 00:32:57 +0800

    UI and button updates
    Fixed buttons and enhanced background image. Also connected start with game scene with the transition screen.

--- scenes/Home.gd
+++ scenes/Home.gd
@@ -14,4 +14,18 @@ func _process(delta: float) -> void:
 func _on_start_pressed() -> void:
 	TransitionScreen.transition()
 	await TransitionScreen.on_transition_finished
-	get_tree().change_scene_to_file("res://scenes/world_map.tscn")
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+
+
+func _on_ranking_pressed() -> void:
+	print("ranking")
+
+
+func _on_credits_pressed() -> void:
+	print("credits")
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
